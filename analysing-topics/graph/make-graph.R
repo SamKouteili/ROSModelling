d72f587e1de98f06a25efa8cb379630f4d30c2d1
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2021-11-07 16:56:47 +0800

    done with the rscript ot make graph

--- analysing-topics/graph/make-graph.R
+++ analysing-topics/graph/make-graph.R
@@ -3,12 +3,13 @@
 # version 07-11-21
 
 # import library  ---------------------------------------------------------------
-library("dplyr") 
-library("ggplot2")
+# library("dplyr") 
+# library("ggplot2")
 
 # setting up data --------------------------------------------------------------
-data <- t(read.csv("timestamp.csv", header = FALSE))
-View(data)
+args <- commandArgs(TRUE)
+data <- t(read.csv(args[1], header = FALSE))
+
 len <- 100 # number of calls to look at
 
 # set up index, x minimum and maximum
@@ -16,7 +17,6 @@ index <- seq(1, ncol(data), 1)
 xlim <- c(1427157864, 1427157867)
 
 # Drawing graph individually with limit in x-axis
-help(png)
 png('timestamp-graph.png', width = 1000, height = 600, units="px")
 plot(x=data[1:len,1], xlim = xlim, 
      y = rep(index[1], len), ylim = c(1, ncol(data)),
@@ -24,18 +24,18 @@ plot(x=data[1:len,1], xlim = xlim,
 for (i in 2:length(index)) {
   points(x = data[1:len, i], y = rep(index[i], len), pch = 6)
 }
-grid(10, NULL, lty = 2)
+grid(NULL, NULL)
 dev.off()
 
 
 # using ggplot without limit in x-axis
-for (i in 2:ncol(data)) {
-  print(i)
-}
-help(ggplot)
-ggplot() + 
-  scale_shape_manual(values = 12) +
-  geom_point(aes(x = data[1:len, 1], y = rep(1, len))) + 
-  geom_point(aes(x = data[1:len, 2], y = rep(1.1, len))) + 
-  geom_point(aes(x = data[1:len, 3], y = rep(1.2, len)))
+# for (i in 2:ncol(data)) {
+#   print(i)
+# }
+# help(ggplot)
+# ggplot() + 
+#   scale_shape_manual(values = 12) +
+#   geom_point(aes(x = data[1:len, 1], y = rep(1, len))) + 
+#   geom_point(aes(x = data[1:len, 2], y = rep(1.1, len))) + 
+#   geom_point(aes(x = data[1:len, 3], y = rep(1.2, len)))
   
